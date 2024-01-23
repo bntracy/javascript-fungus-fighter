e1c93979f2a5a9cfea93e071b4fc80b8ca12d7a9
Beth Tracy <bntracy@gmail.com>
2024-01-22 18:32:45 -0600

    base functionality complete

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let userAP = 100;
+
 function onReady() {
-    console.log("Ready to go!")
+    // console.log("Ready to go!")
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -14,7 +17,54 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    renderAP();
+    renderHP();
 }
 
 
 onReady()
+
+function attack(apLost, hpLost) {
+  console.log('button clicked');
+  userAP -= apLost;
+  if (userAP < 0) {
+    userAP = 0;
+  }
+  fungusHP -= hpLost;
+  if (fungusHP < 0) {
+    fungusHP = 0;
+  }
+  renderAP();
+  renderHP();
+  if (fungusHP == 0) {
+    userWins();
+  } else if (userAP == 0) {
+    fungusWins();
+  }
+}
+
+function renderAP() {
+    document.querySelector('.ap-text').innerHTML = userAP + ' AP';
+}
+
+function renderHP() {
+    document.querySelector('.hp-text').innerHTML = fungusHP + ' HP';
+}
+
+function userWins() {
+    // fungus goes boom
+    document.querySelector('.freaky-fungus').classList.remove('walk');
+    document.querySelector('.freaky-fungus').classList.add('dead');
+}
+
+function fungusWins() {
+    // fungus rejoices
+    document.querySelector('.freaky-fungus').classList.remove('walk');
+    document.querySelector('.freaky-fungus').classList.add('jump');
+    // disable the buttons
+    document.querySelector('.arcane-scepter').disabled = true;
+    document.querySelector('.entangle').disabled = true;
+    document.querySelector('.dragon-blade').disabled = true;
+    document.querySelector('.star-fire').disabled = true;
+}
